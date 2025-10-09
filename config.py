71b36950336253398d4d58ea17def59c019bc9eb
Japanese samurai <parsabotcreator@gmail.com>
2025-10-09 10:13:53 +0330

    some develop

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 TOKEN = "write the token"
 admin = [] # id of your admins here
-welcome_text = "hello this bot in under dev" # your welcome text
+welcome_text = "hello, please send your message:" # your welcome text
+chanel_of_messages = "@write_like_it"
+banned_users = []
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import telebot
-from telebot import types
 from telebot import TeleBot
 from config import *
 
@@ -7,8 +6,32 @@ bot = TeleBot(token=TOKEN)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
-    bot.send_message(message.chat.id, text=welcome_text)
+    global message_one
+    message_one = bot.send_message(message.chat.id, text=welcome_text)
+    bot.register_next_step_handler(message_one, send_to_chanel_def, sendchanel, send_confrim)
 
-@bot.send_message(commands=['sendtouser'])
-def sendtouser_handler(message)
-    bot.reply_to(message, "please wait to fully develop its start-up")
+def send_to_chanel_def(message):
+    user_id = message.from_user.id
+    global sendchanel
+    sendchanel = bot.send_message(chanel_of_messages, """new message:
+    {message_one} from {user_id}""")
+    global send_confrim
+    send_confrim = bot.send_message(message.chat.id, "your message sended for admins")
+
+
+@bot.message_handler(commands=['send_user'])
+def send_to_user(message):
+    user_id = message.from_user.id
+    if user_id in admin:
+        global get_user_to_send
+        get_user_to_send = bot.send_message(message.chat.id, "send the user_id to send message to user you selected")
+        bot.register_next_step_handler(get_user_to_send,sendmessagetouserdef, getmessagefromadmin, sendtouserthetext, sendconfrimtoadmin)
+def sendmessagetouserdef(message):
+    global getmessagefromadmin
+    getmessagefromadmin = bot.send_message(message.chat.id, "send your message to user")
+    global sendtouserthetext
+    sendtouserthetext = bot.send_message(get_user_to_send, "you have new message from admins: {getmessagefromadmin}")
+    global sendconfrimtoadmin
+    sendconfrimtoadmin = bot.send_message(message.chat.id, "sended")
+        
+    
